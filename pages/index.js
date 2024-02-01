34a7a54d32538b16f1cfc7f943d7bca37f1a45fd
nkayilovette03 <nkayilovette03@gmail.com>
2024-02-01 00:47:29 -0500

    Basic of ssr in nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,41 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+// ////// BUT WITH SSR THIS HOW YOU DO IT //////////////
+
+export async function getServerSideProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+  const data = await res.json();
+
+  return {
+    props: {
+      todos: data,
+    },
+  };
+}
+
+export default function Home({ todos }) {
+  //////// NORMALLY THIS IS HOW YOU WILL CALL AN API AND RENDER TO A WEBPAGE ////////////
+
+  // const [todos, setTodos] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchTodos = async () => {
+  //     setTimeout(async () => {
+  //       const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+  //       const data = await res.json();
+  //       setTodos(data);
+  //     }, 0);
+  //   };
+
+  //   fetchTodos();
+  // }, []);
+
   return (
     <>
       <Head>
@@ -14,100 +44,29 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+        {todos?.length == 0 ? (
+          <div>Loading....</div>
+        ) : (
+          todos?.map((todo) => (
+            <div
+              style={{
+                background: "red",
+                padding: "4px 10px",
+                margin: "4px 0px",
+                borderRadius: "5px",
+                display: "block",
+                alignSelf: "flex-start",
+              }}
+              key={todos.id}
             >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+              <p>
+                {todo.id}: {todo.title}
+              </p>
+            </div>
+          ))
+        )}
       </main>
     </>
   );
